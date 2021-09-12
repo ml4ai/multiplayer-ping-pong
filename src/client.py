ed08fdf2d5d0f56c80af5f9be69cc36b2fb5c2bb
Eric Duong <86935179+eduongAZ@users.noreply.github.com>
2021-09-11 18:41:37 -0700

    Set initial client window (#6)

---
+++ src/client.py
@@ -0,0 +1,42 @@
+import pygame
+
+WINDOW_SIZE = (1100, 800)
+
+if __name__ == "__main__":
+    pygame.init()
+
+    # Set up game window
+    screen = pygame.display.set_mode(WINDOW_SIZE)
+    pygame.display.set_caption("Multiplayer Ping Pong")
+
+    # Use clock to control framerate
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+        # Exit the game if user hits close
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        if not running:
+            break
+
+        # Send control commands to server
+        keys = pygame.key.get_pressed()
+
+        # TODO: send control commands to server
+        if keys[pygame.K_UP]:
+            print("UP pressed")
+        elif keys[pygame.K_DOWN]:
+            print("DOWN pressed")
+        
+        # Draw background
+        screen.fill((0, 0, 0))
+
+        # Update screen
+        pygame.display.flip()
+
+        # Limit to 60 frames per second
+        clock.tick(60)
+
+    pygame.quit()
