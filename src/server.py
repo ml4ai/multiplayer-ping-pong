c09ed216c1cb02d749b0b650fbb7c56b67ce373f
eduongAZ <eduong@email.arizona.edu>
2021-09-12 13:00:02 -0700

    turn server into a class

--- src/server.py
+++ src/server.py
@@ -4,34 +4,40 @@ from utils import Network
 
 PORT = 5050
 
-def handle_client(connection, address):
-    print("[SERVER] connected to: ", address)
-    client_network = Network(connection)
+class Server:
+    def __init__(self):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = socket.gethostbyname(socket.gethostname())
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Reuse socket
+        self.server.bind((host, PORT))
+        self.server.listen()
+        print(f"[NETWORK] ({host}, {PORT})")
 
-    while True:
-        try:
-            data = client_network.receive()
+    def run(self):
+        while True:
+            client_conn, client_addr = self.server.accept()
+            client_thread = threading.Thread(target=self.handle_client, args=(client_conn, client_addr))
+            client_thread.start()
 
-            if data == 'x':
+    def handle_client(self, connection, address):
+        print("[SERVER] connected to: ", address)
+        client_network = Network(connection)
+
+        while True:
+            try:
+                data = client_network.receive()
+
+                if data == 'x':
+                    break
+
+                print(data)
+            except socket.error as err:
+                print(err)
                 break
 
-            print(data)
-        except socket.error as err:
-            print(err)
-            break
-
-    print("Connection lost: ", address)
-    connection.close()
+        print("Connection lost: ", address)
+        connection.close()
 
 if __name__ == "__main__":
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = socket.gethostbyname(socket.gethostname())
-    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Reuse socket
-    server.bind((host, PORT))
-    server.listen()
-    print(f"[NETWORK] ({host}, {PORT})")
-
-    while True:
-        client_conn, client_addr = server.accept()
-        client_thread = threading.Thread(target=handle_client, args=(client_conn, client_addr))
-        client_thread.start()
+    server = Server()
+    server.run()
