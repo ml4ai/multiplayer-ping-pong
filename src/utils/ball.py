4faed299fd5248ec029ce4d16b23c388956626b0
eduongAZ <eduong@email.arizona.edu>
2021-09-18 13:21:43 -0700

    configuration file

--- src/utils/ball.py
+++ src/utils/ball.py
@@ -1,29 +1,25 @@
 import pygame
+import config as cfg
 from random import randint
 from typing import Optional
 from math import copysign
 
-BALL_SIZE = 10
-WHITE = (255, 255, 255)
-BALL_X_SPEED = 6
 
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
 
-        self.image = pygame.Surface([BALL_SIZE, BALL_SIZE])
+        self.image = pygame.Surface([cfg.BALL_SIZE, cfg.BALL_SIZE])
         self.image.fill((0, 0, 0))
         self.image.set_colorkey((0, 0, 0))
 
-        pygame.draw.rect(self.image, WHITE, [0, 0, BALL_SIZE, BALL_SIZE])
+        pygame.draw.rect(self.image, cfg.FOREGROUND_COLOR, [0, 0, cfg.BALL_SIZE, cfg.BALL_SIZE])
 
-        self.velocity = [BALL_X_SPEED, randint(-8, 8)]
+        self.velocity = [cfg.BALL_X_SPEED, randint(-8, 8)]
 
         self.rect = self.image.get_rect()
         self.rect.x = 545
         self.rect.y = 395
-
-        self.size = BALL_SIZE
 
     def update(self):
         self.rect.x += self.velocity[0]
